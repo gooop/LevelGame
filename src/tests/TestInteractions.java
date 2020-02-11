0bfbca4653cb5d5ce43cbd7e4046d7472e03ccce
gooop <gcastaneda456@gmail.com>
2020-02-10 17:27:04 -0700

    Tests done

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -7,42 +7,79 @@ import org.junit.Test;
 import gameEngine.Drawable;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
-import levelPieces.Knight;
+import levelPieces.*;
 
 public class TestInteractions {
 	int playerLoc = (GameEngine.BOARD_SIZE - 1) / 2; // Middle of board
 	
 	@Test
 	public void testArcherInteract() {
-		
-		fail("Not yet implemented");
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			Archer archer = new Archer('A', i);
+			gameBoard[i]= archer; 
+			if (Math.abs(playerLoc - i) != 2) {
+				assertEquals(InteractionResult.NONE, archer.interact(gameBoard, playerLoc));
+			} else {
+				assertEquals(InteractionResult.KILL, archer.interact(gameBoard, playerLoc));
+			}
+		}
 	}
 	
 	@Test
 	public void testKnightInteract() {
-		Knight [] gameBoard = new Knight[GameEngine.BOARD_SIZE];
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-			gameBoard[i] = new Knight('E', i);
+			Knight knight = new Knight('E', i);
+			gameBoard[i]= knight; 
 			if (i != playerLoc) {
-				assertEquals(InteractionResult.NONE, gameBoard[i].interact(gameBoard, playerLoc));
+				assertEquals(InteractionResult.NONE, knight.interact(gameBoard, playerLoc));
+			} else {
+				assertEquals(InteractionResult.KILL, knight.interact(gameBoard, playerLoc));
 			}
 		}
-		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void testBomberInteract() {
-		fail("Not yet implemented");
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			Bomber bomber= new Bomber('B', i);
+			gameBoard[i]= bomber; 
+			if (i != playerLoc) {
+				assertEquals(InteractionResult.NONE, bomber.interact(gameBoard, playerLoc));
+			} else {
+				assertEquals(InteractionResult.HIT, bomber.interact(gameBoard, playerLoc));
+			}
+		}
 	}
 	
 	@Test
 	public void testBombInteract() {
-		fail("Not yet implemented");
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			Bomb bomb= new Bomb('B', i);
+			gameBoard[i]= bomb; 
+			if (i != playerLoc) {
+				assertEquals(InteractionResult.NONE, bomb.interact(gameBoard, playerLoc));
+			} else {
+				assertEquals(InteractionResult.KILL, bomb.interact(gameBoard, playerLoc));
+			}
+		}
 	}
 	
 	@Test
 	public void testGoldInteract() {
-		fail("Not yet implemented");
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			Gold gold = new Gold('G', i);
+			gameBoard[i]= gold; 
+			if (i != playerLoc) {
+				assertEquals(InteractionResult.NONE, gold.interact(gameBoard, playerLoc));
+			} else {
+				assertEquals(InteractionResult.GET_POINT, gold.interact(gameBoard, playerLoc));
+			}
+		}
 	}
 
 }
